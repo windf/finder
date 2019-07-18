d4ee25efe8849c216c6e75270a77b0373266b8eb
linyu <linyu@laoyuegou.com>
2019-07-18 17:50:06 +0800

    update

--- controllers/record.go
+++ controllers/record.go
@@ -1,19 +1,23 @@
 package controllers
 
 import (
+	"finder/model"
 	"github.com/labstack/echo"
 	"net/http"
 )
 
 func GetList(c echo.Context) error {
+	return c.JSON(http.StatusOK, nil)
+}
+
+func GetDetail(c echo.Context) error {
 	result, err := findSrv.GetRecord(1)
 	if err != nil {
 		findSrv.Logger.Errorf("get record err:%s", err.Error())
 	}
 
+	if result == nil {
+		result = new(model.Record)
+	}
 	return c.JSON(http.StatusOK, result)
-}
-
-func GetDetail(c echo.Context) error {
-	return c.JSON(http.StatusOK, nil)
 }
