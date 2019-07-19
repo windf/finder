429f5f3f32bc8310f2718d1da70cca1c6d0982c5
linyu <linyu@laoyuegou.com>
2019-07-19 10:58:06 +0800

    update

--- controllers/init.go
+++ controllers/init.go
@@ -9,6 +9,10 @@ import (
 
 var findSrv *service.Service
 
+type GetDetailReq struct {
+	ID int64 `json:"id" form:"id" query:"id"`
+}
+
 type Message struct {
 	Errcode int         `json:"errcode" xml:"errcode"`
 	Errmsg  string      `json:"errmsg,omitempty" xml:"errmsg"`
@@ -35,10 +39,17 @@ func Init(c *config.Config, s *service.Service) {
 	s.Echo.Logger.Fatal(s.Echo.Start(c.HttpAddress))
 }
 
-func JSON(c echo.Context, code int, msg string, i interface{}) error {
+func JsonSuccess(c echo.Context, code int, i interface{}) error {
+	return c.JSON(code, &Message{
+		Errcode: code,
+		Errmsg:  "success",
+		Data:    i,
+	})
+}
+
+func JsonFail(c echo.Context, code int, msg string) error {
 	return c.JSON(code, &Message{
 		Errcode: code,
 		Errmsg:  msg,
-		Data:    i,
 	})
 }
--- controllers/record.go
+++ controllers/record.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"finder/model"
 	"github.com/labstack/echo"
 	"net/http"
 )
@@ -10,15 +9,27 @@ func GetList(c echo.Context) error {
 	return c.JSON(http.StatusOK, nil)
 }
 
-func GetDetail(c echo.Context) error {
+func GetDetail(c echo.Context) (err error) {
+	req := new(GetDetailReq)
+	if err = c.Bind(req); err != nil {
+		return
+	}
+
+	if req.ID <= 0 {
+		return JsonFail(c, http.StatusOK, "id不正确")
+	}
+
 	result, err := findSrv.GetRecord(1)
 	if err != nil {
 		findSrv.Logger.Errorf("get record err:%s", err.Error())
 	}
 
-	if result == nil {
-		result = new(model.Record)
+	var res interface{}
+	res = struct{}{}
+
+	if result != nil {
+		res = result
 	}
 
-	return JSON(c, http.StatusOK, "success", result)
+	return JsonSuccess(c, http.StatusOK, res)
 }
