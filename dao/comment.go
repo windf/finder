da2dc6ec5a8bb27871ecdb6f02761f7cd4ac9936
linyu <linyu@laoyuegou.com>
2019-07-19 15:32:19 +0800

    update

--- dao/comment.go
+++ dao/comment.go
@@ -11,7 +11,7 @@ const (
 
 func (d *Dao) GetCommentList(recordId int64, page, pageSize int) (result []*model.Comment, err error) {
 	offset := (page - 1) * pageSize
-	err = d.dbr.Table(_recordTable).Where("record_id=?", recordId).Offset(offset).Limit(pageSize).Select("*").Find(&result).Error
+	err = d.dbr.Table(_commentTable).Where("record_id=?", recordId).Offset(offset).Limit(pageSize).Select("*").Find(&result).Error
 	if err != nil {
 		if err == gorm.ErrRecordNotFound {
 			result = nil
