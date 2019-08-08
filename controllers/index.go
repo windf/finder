f24748135497d8d64f9f7caedbb22080a1eb5e2f
linyu <linyu@laoyuegou.com>
2019-08-08 11:28:03 +0800

    update

--- controllers/index.go
+++ controllers/index.go
@@ -6,5 +6,19 @@ import (
 )
 
 func RenderIndex(c echo.Context) error {
-	return c.Render(http.StatusOK, "index", nil)
+	res := map[string]interface{}{
+		"title": "首页",
+		"data":  nil,
+		//"totalPage": totalPage,
+		//"pageSize":  pageSize,
+		//"page":      page,
+		//"isFind":    isFind,
+		//"status":    status,
+	}
+	/*
+		if result != nil {
+			res["data"] = result
+		}*/
+
+	return c.Render(http.StatusOK, "index", res)
 }
