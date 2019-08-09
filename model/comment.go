28eb5352cf54c9b7675a80fb60d0862d3142e885
linyu <linyu@laoyuegou.com>
2019-08-09 14:25:26 +0800

    update

--- model/comment.go
+++ model/comment.go
@@ -7,5 +7,5 @@ type Comment struct {
 	Photo      string `json:"photo" gorm:"column:photo"`
 	Remark     string `json:"remark" gorm:"column:remark"`
 	CreateTime int64  `json:"create_time" gorm:"column:create_time"`
-	ShowTime   string `json:"show_time"`
+	ShowTime   string `gorm:"-"`
 }
